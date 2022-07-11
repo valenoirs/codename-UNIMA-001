8849c6335bfa1c5ed1f0465f5e2fd457627d272c
valenoirs <luckyvw.general@gmail.com>
2022-07-12 00:50:39 +0800

    fix server crash if user manually type url

--- routes/view.js
+++ routes/view.js
@@ -3,20 +3,32 @@ const router = require('express').Router()
 const Agenda = require('../models/agenda')
 
 router.get('/', async (req, res) => {
-        const agenda = await Agenda.find();
+    const agenda = await Agenda.find();
 
-        res.render('user/index', {title: 'Home', layout: 'user/layout/main', error: req.flash('error'), agenda})
+    res.render('user/index', {title: 'Home', layout: 'user/layout/main', error: req.flash('error'), agenda})
 })
 
-router.get('/:id', async (req, res) => {
-        if(!req.session.userId){
-            res.redirect('/')
+router.get('/:id', async (req, res, next) => {
+    if(!req.session.userId){
+        res.redirect('/')
+    }
+    else{
+        try {
+            const agenda = await Agenda.findById(req.params.id)
+    
+            if(!agenda){
+                console.log('agenda not found!')
+                req.flash('error', 'Agenda tidak ditemukan.')
+                return res.redirect('/')
+            }
+    
+            res.render('user/detail', {title: agenda.name, layout: 'user/layout/main', error: req.flash('error'), agenda})
+        } catch (error) {
+            console.error('agenda not found!', error)
+            req.flash('error', 'Agenda tidak ditemukan.')
+            return res.redirect('/')
         }
-        else{
-			const agenda = await Agenda.findById(req.params.id)
-			
-			res.render('user/detail', {title: agenda.name, layout: 'user/layout/main', error: req.flash('error'), agenda})
-        }
+    }
 })
 
 module.exports = router
